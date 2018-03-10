7be29d04f0f0fe7cbc16c1d47f9a79ee8ae56000
shoter <lubiebardzozelki@gmail.com>
2018-03-10 12:44:53 +0100

    Changes to the project.

--- backend/ConsoleTests/Program.cs
+++ backend/ConsoleTests/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities
+namespace ConsoleTests
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
