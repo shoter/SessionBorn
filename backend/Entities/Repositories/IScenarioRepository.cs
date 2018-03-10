fe7d8fbe3358bba08b920fd9efb3a69fdf161e12
mtokarz <34172570+mtokarz@users.noreply.github.com>
2018-03-10 16:42:21 +0100

    scenarios and quests  repos and services added

---
+++ backend/Entities/Repositories/IScenarioRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.EntityFramework;
+
+namespace Entities.Repositories
+{
+    public interface IScenarioRepository : IRepository<Scenario>
+    {
+        List<Scenario> GetUsersScenarios(int userID);
+        Scenario GetScenario(int scenarioID);
+
+        
+    }
+}
---
+++ backend/Entities/Repositories/QuestRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.EntityFramework;
+
+namespace Entities.Repositories
+{
+    class QuestRepository : RepositoryBase<Quest, SessionBornEntities>, IQuestRepository
+    {
+        public QuestRepository(SessionBornEntities context) : base(context)
+        {
+        }
+
+        public List<Quest> GetScenarioQuests(int scenarioID)
+        {
+            return Where(x => x.ScenarioID == scenarioID).ToList();
+        }
+        public Quest GetQuest(int questID)
+        {
+            return SingleOrDefault(x => x.ID == questID);
+        }
+
+     }
+}
