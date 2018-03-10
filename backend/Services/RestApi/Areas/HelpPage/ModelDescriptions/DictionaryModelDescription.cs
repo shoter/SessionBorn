1823bae904208ea8bf261801750df39f8ec977d7
shoter <lubiebardzozelki@gmail.com>
2018-03-10 12:33:10 +0100

    Added again backend because i F*cked up xD

--- backend/Services/RestApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RestApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- backend/Services/RestApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RestApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
