8f1b89d1ad701c1f9da815acd04945c04223cab2
KrzysztofStachanczyk <ksstach@gmail.com>
2018-03-10 04:57:06 -0800

    Navbar

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,9 +6,13 @@ import App from './App'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vue-awesome/icons'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
+import Icon from 'vue-awesome/components/Icon'
+Vue.component('icon', Icon)
 
 /* eslint-disable no-new */
 new Vue({
