8b5a28e42c9fb7a9adfec6dba26b30be9adbc712
KrzysztofStachanczyk <ksstach@gmail.com>
2018-03-11 01:07:21 -0800

    Map bug fix

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -52,36 +52,29 @@
           }]
         this.center = pos
       } else {
-        this.$http.get('http://arrowtotherest.azurewebsites.net/api/Sidebar', {
+        this.$http.get('http://arrowtotherest.azurewebsites.net/api/Location', {
           headers: {
             Authorization: 'Bearer ' + this.$cookie.get('skyrim_token')
           }
         }).then(response => {
           this.markers = []
           let self = this
-          let res = [{
-            'name': 'sample string 2',
-            'type': 'sample string 4',
-            'Latitude': 1.0,
-            'Longitude': 1.0,
-            'completed': false,
-            'description': 'sample string 8',
-            'points': 9
-          }]
-          res.forEach(function (quest) {
-            if (quest.Latitude != null && quest.Longitude != null && quest.completed === false) {
+          console.log(response.body)
+          response.body.forEach(function (quest) {
+            if (quest.latitude != null && quest.longitude != null) {
               console.log(self)
               self.markers.push({
-                position: {lat: quest.Latitude, lng: quest.Longitude},
-                title: quest.name,
+                position: {lat: quest.latitude, lng: quest.longitude},
+                title: '[' + quest.scenarioTitle + ']' + quest.questTitle,
                 desc: quest.description,
                 messageVisible: true
               })
-              if (self.markers.length > 0) {
-                self.center = self.markers[0].position
-              }
             }
           })
+
+          if (self.markers.length > 0) {
+            self.center = self.markers[0].position
+          }
         }, response => {
           this.$snotify.error('Unable to download map data', 'Error')
           this.error = response.error
